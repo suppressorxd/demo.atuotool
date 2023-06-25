93e483c0027842a2402dc47fc517c834a50759d3
suppressorxd <126715386+suppressorxd@users.noreply.github.com>
2023-06-25 23:08:19 +0300

    demo auto tool builder
    demo auto tool
    builder

---
+++ main.py
@@ -0,0 +1,33 @@
+import os 
+
+print('TurkHackTeam/Suppressor')
+
+os.system('apt update -y')
+
+os.system('apt install git -y')
+
+os.system('apt install subfinder -y')
+
+os.system('apt install subjack -y')
+
+os.system('apt install airgeddon -y')
+
+os.system('apt install uniscan -y')
+
+os.system('apt install recon -y')
+
+os.system('apt install nikto -y')
+
+os.system('apt install lynis -y')
+
+os.system('git clone https://github.com/mentalistler/FetiSQL-V2.git')
+
+os.system('git clone https://github.com/joaomatosf/jexboss.git')
+
+print('TurkHackTeam/Suppressor')
+
+
+
+
+
+
